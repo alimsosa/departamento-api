2d6e596e8f882bb29dc4e9b9c0c8ecb6e26b67b8
alimsosa <alimsosa98@gmail.com>
2022-05-15 19:44:12 -0300

    registro de votos

--- src/main/java/com/departamentoapi/demo/controller/VotosController.java
+++ src/main/java/com/departamentoapi/demo/controller/VotosController.java
@@ -1,7 +1,31 @@
 package com.departamentoapi.demo.controller;
 
+import com.departamentoapi.demo.domain.VotedOkDTO;
+import com.departamentoapi.demo.domain.VoteDTO;
+import com.departamentoapi.demo.services.VotosService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class VotosController {
+
+    private final VotosService votosService;
+
+    @GetMapping("/getvotos")
+    public ResponseEntity<VoteDTO> getVotos(@RequestBody VoteDTO voto) {
+        VoteDTO response = votosService.getVotos();
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @PostMapping("/vote")
+    public ResponseEntity<VotedOkDTO> createVote(@RequestBody VoteDTO voto) {
+        VotedOkDTO response = votosService.createVote(voto);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/departamentoapi/demo/domain/VotedOkDTO.java
@@ -0,0 +1,14 @@
+package com.departamentoapi.demo.domain;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class VotedOkDTO {
+    private String confirmation;
+}
